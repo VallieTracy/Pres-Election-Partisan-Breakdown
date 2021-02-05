82f417b4bc901bea89f636008df4f6d6b178175f
Melissa Keller <kelle595@umn.edu>
2021-02-05 14:34:35 -0600

    well, it's not 100% right, but it's not not working after integrating Kyle's code into the counties_app.py file

--- counties_app.py
+++ counties_app.py
@@ -24,18 +24,15 @@ app = dash.Dash(__name__, requests_pathname_prefix = '/counties/', serve_locally
 
 countiesDF = pd.read_csv("Resources/countypres_2000-2016.csv")
 
-
-df2016 = countiesDF[(countiesDF["year"]==2016)]
-
-df2016_reps = df2016[df2016.party == "republican"]
-
-df2016_reps['percent'] = df2016_reps['candidatevotes'] / df2016_reps['totalvotes']
-
+df2016 = countiesDF[(countiesDF["year"] == 2016)]
+df2016_reps = df2016[(df2016["party"]) == "republican"]
+df2016_reps["percent"] = df2016_reps["candidatevotes"] / df2016["totalvotes"]
 
 fips = df2016_reps["FIPS"]
 values = df2016_reps["percent"]
 valuesPop = df2016_reps["totalvotes"]
 
+endptsPop = list(np.mgrid[min(valuesPop):max(valuesPop):6j])  # Do we need this?
 
 colorscale = [ 'hsl(240,50%,50%)', 'hsl(300,50%,50%)','hsl(0,50%,50%)',\
              
@@ -43,12 +40,8 @@ colorscale = [ 'hsl(240,50%,50%)', 'hsl(300,50%,50%)','hsl(0,50%,50%)',\
               
              'hsl(240,80%,50%)', 'hsl(300,80%,50%)','hsl(0,80%,50%)',\
 
-              
-              'hsl(240,95%,50%)', 'hsl(300,95%,50%)','hsl(0,95%,50%)'\
+              'hsl(240,95%,50%)', 'hsl(300,95%,50%)','hsl(0,95%,50%)'
              ]
-
-endptsPop = list(np.mgrid[min(valuesPop):max(valuesPop):6j])
-
 
 def label_pct(row):
     if row["percent"] <= .20:
@@ -67,23 +60,20 @@ def pop_rank(row):
 def combined_rank(row):
     return row["pop_rank"]*100+row["pct_rank"]
 
-pd.qcut(df2016_reps["totalvotes"], q=5,labels =[1,2,3,4,5], retbins =True)
+df2016_reps["pct_rank"] = df2016_reps.apply (lambda row: label_pct(row), axis=1)
+
 df2016_reps["pop_rank"] = pd.qcut(df2016_reps["totalvotes"], q=[0, .25, .5, .75, 1],labels =[1,2,3,4] )
-df2016_reps['pop_rank'] = df2016_reps['pop_rank'].astype(int)
+d#f2016_reps['pop_rank'] = df2016_reps['pop_rank'].astype(int)
 
+df2016_reps["combined_rank"] = df2016_reps.apply (lambda row: combined_rank(row), axis=1)
 
-df2016_reps["pct_rank"]=df2016_reps.apply (lambda row: label_pct(row), axis=1)
-#df2016_reps["pop_rank"]= df2016_reps.apply (lambda row: pop_rank(row), axis=1) #Causes a str to int error when I try to run this line
-df2016_reps["combined_rank"]= df2016_reps.apply (lambda row: combined_rank(row), axis=1)
-
-#I think color needs to be on the combined rank....
 fig = px.choropleth_mapbox(df2016_reps, geojson=counties, locations='FIPS', color='totalvotes',
                         color_continuous_scale=colorscale,
-                        #range_color=(0, 300000),
-                        mapbox_style="carto-positsron",
+                        range_color=(0, 300000),
+                        mapbox_style="carto-positron",
                         zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
                         opacity=0.5,
-                        labels={'Total Votes': 'totalvotes'}
+                        labels={'Total Votes':'totalvotes'}
                         )
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 app.layout = html.Div(children=[
@@ -99,6 +89,7 @@ app.layout = html.Div(children=[
     )
 ])
 
+#######################
 # The below code doesn't not work, so I'm leaving it to reference back to as I embarque
 # on testing out the new stuff.
 
