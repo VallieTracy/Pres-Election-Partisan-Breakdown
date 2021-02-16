a6d4e366b818e15ab1c631adb496c45d53487407
Melissa Keller <kelle595@umn.edu>
2021-02-15 22:08:51 -0600

    moved some code from .py files to final_code JN, tried to edit legend and css but that failed.

--- counties_app.py
+++ counties_app.py
@@ -9,7 +9,6 @@ import pandas as pd
 import numpy as np
 from urllib.request import urlopen
 import json
-#import plotly.figure_factory as ff
 
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
@@ -22,13 +21,8 @@ app = dash.Dash(__name__, requests_pathname_prefix = '/counties/', serve_locally
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
-countiesDF = pd.read_csv("Resources/countypres_2000-2016.csv")
+df2016_reps = pd.read_csv("Resources/usa_counties.csv")
 
-df2016 = countiesDF[(countiesDF["year"] == 2016)]
-df2016_reps = df2016[(df2016["party"]) == "republican"]
-df2016_reps["percent"] = df2016_reps["candidatevotes"] / df2016["totalvotes"]
-
-df2016_reps = df2016_reps.dropna(subset = ["FIPS"])
 df2016_reps["FIPS"] = df2016_reps["FIPS"].astype(np.int64)
 df2016_reps["FIPS"] = df2016_reps["FIPS"].astype(str)
 
@@ -37,13 +31,11 @@ while i < len(df2016_reps.index):
 
     if len(df2016_reps.iloc[i,4])<=4:
         df2016_reps.iloc[i,4] = "0" + df2016_reps.iloc[i,4]
+        #print(df2016_reps.iloc[i,4])
         i+=1
     else:
         i+=1
 
-# fips = df2016_reps["FIPS"]
-# values = df2016_reps["percent"]
-# valuesPop = df2016_reps["totalvotes"]
 
 colorscale = {'104' : 'hsl(240,50%,50%)', '106': 'hsl(300,50%,50%)', '110': 'hsl(0,50%,50%)',
              '204': 'hsl(240,65%,50%)', '206': 'hsl(300,65%,50%)', '210': 'hsl(0,65%,50%)',
@@ -51,27 +43,11 @@ colorscale = {'104' : 'hsl(240,50%,50%)', '106': 'hsl(300,50%,50%)', '110': 'hsl
              '404': 'hsl(240,95%,50%)', '406': 'hsl(300,95%,50%)', '410': 'hsl(0,95%,50%)'
 }
 
-# instead of doing all this in here, and even the trimming the dataframe down to reps
-# and year 2016, should we do that all in JN and then export to csv, and then this file
-# uses that cleaned up CSV, to save processing time on this?
-def label_pct(row):
-    if row["percent"] <= .40:
-        return 4
-
-    if row["percent"] <= .60:
-        return 6
-
-    return 10
-
-def pop_rank(row):
-    return pd.qcut(row,5, retbins = True)
-
-def combined_rank(row):
-    return row["pop_rank"]*100+row["pct_rank"]
-
-df2016_reps["pct_rank"] = df2016_reps.apply (lambda row: label_pct(row), axis=1)
-df2016_reps["pop_rank"] = pd.qcut(df2016_reps["totalvotes"], q=[0, .25, .5, .75, 1],labels =[1,2,3,4] )
-df2016_reps["combined_rank"] = df2016_reps.apply (lambda row: combined_rank(row), axis=1)
+legend_names = {'104' : 'Low Pop, Low Rep', '106': 'Low Pop, Med Rep', '110': 'Low Pop, High Rep',
+             '204': 'Med Low Pop, Low Rep', '206': 'Med Low Pop, Med Rep', '210': 'Med Low Pop, High Rep',
+             '304': 'Med High Pop, Low Rep', '306': 'Med High Pop, Med Rep', '310': 'Med High Pop, High Rep',
+             '404': 'High Pop, Low Rep', '406': 'High Pop, Med Rep', '410': 'High Pop, High Rep'
+}
 
 df2016_reps["combined_rank"] = df2016_reps["combined_rank"].astype(str)
 
@@ -81,9 +57,11 @@ fig = px.choropleth_mapbox(df2016_reps, geojson=counties, locations='FIPS', colo
                         zoom=3.25, center = {"lat": 37.0902, "lon": -95.7129},
 
                         opacity=1,
-                        #labels={'totalvotes':'Total Votes'}
+                        labels={'totalvotes':'Total Votes', 'percent': "Precent"}
                         )
-                        
+
+
+fig.update_traces(selector = legend_names)
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 app.layout = html.Div(children=[
        html.Link(
@@ -92,7 +70,6 @@ app.layout = html.Div(children=[
     ),
 
     dcc.Graph(
-        # id='counties-graph',
         id="mapContainer",
         figure=fig
     )
--- precinct_app.py
+++ precinct_app.py
@@ -1,6 +1,6 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
-from counties_app import label_pct, pop_rank, combined_rank, colorscale
+from counties_app import colorscale, legend_names
 
 import dash
 import dash_core_components as dcc
@@ -24,33 +24,18 @@ app = dash.Dash(__name__, requests_pathname_prefix = '/precinct/', serve_locally
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
-csvpath = "Resources/2016_precincts_newVersion.csv"
-prec_df = pd.read_csv(csvpath, encoding="ISO-8859-1")
-
-prec_df = prec_df[['VTDID', 'PCTNAME', 'PCTCODE', 'MCDNAME', 'COUNTYNAME', 'COUNTYCODE',
-                   'USPRSR', 'USPRSDFL', 'USPRSTOTAL']][:-1]
-
-prec_df = prec_df.rename(columns = {'MCDNAME': 'Munic/Unorg Terr Name',
-                                    'USPRSR': 'Trump', 'USPRSDFL': 'Clinton',
-                                    'USPRSTOTAL': 'Total Votes'})
-
-prec_df['percent'] = prec_df['Trump'] / prec_df['Total Votes']
-
-
-prec_df["pct_rank"] = prec_df.apply (lambda row: label_pct(row), axis=1)
-prec_df["pop_rank"] = pd.qcut(prec_df["Total Votes"], q=[0, .25, .5, .75, 1],labels =[1,2,3,4] )
-prec_df["combined_rank"] = prec_df.apply (lambda row: combined_rank(row), axis=1)
+prec_df = pd.read_csv("Resources/mn_precincts.csv")
 prec_df["combined_rank"] = prec_df["combined_rank"].astype(str)
 
 fig = px.choropleth_mapbox(prec_df, geojson=precincts, locations='VTDID', featureidkey = "properties.PrecinctID", color='combined_rank',
                         color_discrete_map = colorscale,
                         mapbox_style="carto-positron",
-                        zoom=4, center = {"lat": 46.7296, "lon": -94.6859},
+                        zoom=5, center = {"lat": 46.7296, "lon": -94.6859},
                         opacity=1,
                         labels={'Total Votes':'votes'}
                         
                         )
-#fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 app.layout = html.Div(children=[
     html.Link(
         rel='stylesheet',
